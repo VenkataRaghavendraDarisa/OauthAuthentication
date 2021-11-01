63128a4412e7847f74af4df7bf6942d8cbb1dce8
VenkataRaghavendraDarisa <92907158+VenkataRaghavendraDarisa@users.noreply.github.com>
2021-11-01 09:54:23 +0530

    First Version of Oauth
    this is the First Version of Oauth authentication with google

---
+++ src/main/java/com/oauth/authentication/OauthAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oauth/authentication/OauthAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
